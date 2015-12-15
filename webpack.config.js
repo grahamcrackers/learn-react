56a23278ec4fc5889ee0c1d6a39155a8c29cb0e5
Graham Rogers <gmrogers04@gmail.com>
2015-12-15 16:04:01 -0600

    lesson 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: "./app/components/Main.js",
+    entry: "./app/App.js",
     output: {
         filename: "public/bundle.js"
     },
@@ -8,7 +8,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel-core',
+                loader: 'babel',
                 query: {
                     presets: ['react','es2015']
                 }
