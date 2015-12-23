afc1b4b8de2e47efa81d5f9813c9bad4c0a3df7a
grahamcrackers <gmrogers04@gmail.com>
2015-12-23 17:59:52 -0600

    refactoring to utilize stateless function components

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,37 +1,44 @@
-var React = require('react');
-var Router = require('react-router');
+import React from 'react';
 import Repos from './Github/Repos';
 import UserProfile from './Github/UserProfile';
 import Notes from './Notes/Notes';
-var ReactFireMixin = require('reactfire');
-var FireBase = require('firebase');
-import getGithubInfo from '../utils/helpers'
+import getGithubInfo from '../utils/helpers';
+import Rebase from 're-base';
 
-var Profile = React.createClass({
-    mixins: [ReactFireMixin],
-    getInitialState: function(){
-        return {
+const base = Rebase.createClass('https://luminous-fire-154.firebaseio.com');
+
+class Profile extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
             notes: [1,2,3],
             bio: {},
             repos: []
         }
-    },
-    componentDidMount: function(){
-        this.ref = new FireBase('https://luminous-fire-154.firebaseio.com/');
-        this.init(this.props.params.username);
+    }
 
-    },
-    componentWillReceiveProps: function(nextProps){
-        console.log('The next props are: ', nextProps);
-        this.unbind('notes');
+    componentDidMount(){
+
+        this.init(this.props.params.username);
+    }
+
+    componentWillReceiveProps(nextProps){
+        //console.log('The next props are: ', nextProps);
+        base.removeBinding(this.ref);
         this.init(nextProps.params.username);
-    },
-    componentWillUnmount: function(){
-        this.unbind('notes');
-    },
-    init: function(username){
-        var childRef = this.ref.child(username);
-        this.bindAsArray(childRef, 'notes');
+    }
+
+    componentWillUnmount(){
+        base.removeBinding(this.ref);
+    }
+
+    init(username){
+        this.ref = base.bindToState(username, {
+            context: this,
+            asArray: true,
+            state: 'notes'
+        });
 
         getGithubInfo(username)
             .then(function(data){
@@ -40,12 +47,16 @@ var Profile = React.createClass({
                     repos: data.repos
                 })
             }.bind(this));
-    },
-    handleAddNote: function(newNote){
+    }
 
-        this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote);
-    },
-    render: function(){
+    handleAddNote(newNote){
+
+        base.post(this.props.params.username, {
+            data: this.state.notes.concat([newNote])
+        })
+    }
+
+    render(){
         return (
             <div className="row">
                 <div className="col-md-4">
@@ -57,11 +68,11 @@ var Profile = React.createClass({
                 <div className="col-md-4">
                     <Notes username={this.props.params.username}
                            notes={this.state.notes}
-                           addNote={this.handleAddNote}/>
+                           addNote={(newNote) => this.handleAddNote(newNote)}/>
                 </div>
             </div>
         )
     }
-});
+}
 
-module.exports = Profile;
+export default Profile;
